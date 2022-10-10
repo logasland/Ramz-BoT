e5872aef024739651903f2110ac849759dc9958f
logasland <62803820+logasland@users.noreply.github.com>
2022-10-10 14:39:14 +0700

    Update health.js

--- plugins/health.js
+++ plugins/health.js
@@ -11,15 +11,11 @@ type *${usedPrefix}buy potion ${count - user.potion}* to buy 🧃Potion
 `.trim())
     user.potion -= count * 1
     user.health += heal * count
-    m.chat(`
-Successful use of *${count}* 🧃Potion(s)
-`.trim())
-}
 conn.sendMessage(
     m.chat, 
     {
       location: { degreesLatitude: undefined, degreesLongitude: undefined, jpegThumbnail: fs.readFileSync('./media/Maung.jpg') },
-      caption: 'isi darah lagi',
+      caption: 'Successful use of *${count}* 🧃Potion(s)',
       footer: wm,
       buttons: [{buttonId: '.heal', buttonText: {displayText: 'REHEAL🩹'}, type: 1}],
       headerType: 1
